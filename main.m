e300c7b218d34790c7037f3a78c0cc68efe41d5c
Vishal Pant <25136740+vishalpant@users.noreply.github.com>
2017-11-01 23:27:09 +0530

    Add files via upload

---
+++ main.m
@@ -0,0 +1,25 @@
+input_layer_size  = 400;  % 20x20 Input Images of Digits
+hidden_layer_size = 25;   % 25 hidden units
+num_labels = 10;          % 10 labels, from 1 to 10   
+lambda = 1;               % Regularization term
+load('data.mat');
+m = size(X, 1);
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+fprintf('\nInitializing Neural Network Parameters ...\n')
+fprintf('\nTraining Neural Network... \n')
+options = optimset('MaxIter', 50);
+costFunction = @(p) nnCostFunction(p, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X, y, lambda);
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+pred = predict(Theta1, Theta2, X);
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+
+
